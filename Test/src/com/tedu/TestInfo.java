fbcbfa0636574c7a866f44eda2b2fac08d1cb523
Soul-Yu <58032158+Soul-Yu@users.noreply.github.com>
2020-01-03 11:30:07 +0800

    Update TestInfo.java

--- Test/src/com/tedu/TestInfo.java
+++ Test/src/com/tedu/TestInfo.java
@@ -3,6 +3,8 @@ package com.tedu;
 public class TestInfo {
 	public static void main(String[] args) {
 		String name= "小煜";
+		Integer age = 18;
+		String  properties="is my private";
 		System.out.println(name);
 	}
 }
